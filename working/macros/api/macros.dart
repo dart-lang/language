46b4ae7ba8910aae1653115ac8131d60d2b6b5a5
Jacob MacDonald <jakemac@google.com>
2021-11-24 08:54:14 -0800

    Revert back to original macro api style (#1986)
    Opening for discussion.
    
    This PR moves us back towards the original proposal, where there is a different type to implement for each phase, and each declaration type you can run on.
    
    **Advantages**:
    
    - It is easier for an implementation to know up front exactly which phases a given macro will run in.
    - It allows an implementation to do more efficient invalidation safely - since macros can't share state across phases with local variables you are free to run only a specific phase of a given macro.
    - Macros are more composable - you have per-phase functions you can directly delegate work to, instead of only a more general function.
    - We don't have to worry about  users trying to share objects they shouldn't through local variables (`TypeBuilders` etc).
    - Less nesting, avoids the "callback" style which can be a bit messy.
    
    **Disadvantages**:
    
    - You have to delegate each phase explicitly for any macro you are composing with (see changes to the example).
    - There is a disconnect between where you declare something and where you define it.
    
    I also added the missing `VariableDefinition` and `VariableDefinitionBuilder` classes for introspecting/augmenting top level variables.

--- working/macros/api/macros.dart
+++ working/macros/api/macros.dart
@@ -7,34 +7,132 @@ import 'introspection.dart';
 abstract class Macro {}
 
 /// The interface for [Macro]s that can be applied to any top level function,
-/// instance method, or static method.
-abstract class FunctionMacro implements Macro {
-  /// Invoked for any function that is annotated with this macro.
-  FutureOr<void> visitFunction(
-      FunctionDeclaration function, FunctionContext context);
+/// instance method, or static method, and wants to contribute new type
+/// declarations to the program.
+abstract class FunctionTypesMacro implements Macro {
+  FutureOr<void> buildTypesForFunction(
+      FunctionDeclaration function, TypeBuilder builder);
 }
 
-/// The interface for [Macro]s that can be applied to classes.
-abstract class ClassMacro implements Macro {
-  /// Invoked for any class that is annotated with this macro.
-  FutureOr<void> visitClass(ClassDeclaration clazz, ClassContext context);
+/// The interface for [Macro]s that can be applied to any top level function,
+/// instance method, or static method, and wants to contribute new non-type
+/// declarations to the program.
+abstract class FunctionDeclarationsMacro implements Macro {
+  FutureOr<void> buildDeclarationsForFunction(
+      FunctionDeclaration function, DeclarationBuilder builder);
 }
 
-/// The interface for [Macro]s that can be applied to fields.
-abstract class FieldMacro implements Macro {
-  /// Invoked for any field that is annotated with this macro
-  FutureOr<void> visitField(FieldDeclaration field, FieldContext context);
+/// The interface for [Macro]s that can be applied to any top level function,
+/// instance method, or static method, and wants to augment the function
+/// definition.
+abstract class FunctionDefinitionMacro implements Macro {
+  FutureOr<void> buildDefinitionForFunction(
+      FunctionDeclaration function, FunctionDefinitionBuilder builder);
 }
 
-/// The interface for [Macro]s that can be applied to methods.
-abstract class MethodMacro implements Macro {
-  /// Invoked for any method that is annotated with this macro.
-  FutureOr<void> visitMethod(MethodDeclaration method, MethodContext context);
+/// The interface for [Macro]s that can be applied to any top level variable or
+/// instance field, and wants to contribute new type declarations to the
+/// program.
+abstract class VariableTypesMacro implements Macro {
+  FutureOr<void> buildTypesForVariable(
+      VariableDeclaration variable, TypeBuilder builder);
 }
 
-/// The interface for [Macro]s that can be applied to constructors.
-abstract class ConstructorMacro implements Macro {
-  /// Invoked for each constructor annotated with this macro.
-  FutureOr<void> visitConstructor(
-      ConstructorDeclaration constructor, ConstructorContext context);
+/// The interface for [Macro]s that can be applied to any top level variable or
+/// instance field and wants to contribute new non-type declarations to the
+/// program.
+abstract class VariableDeclarationsMacro implements Macro {
+  FutureOr<void> buildDeclarationsForVariable(
+      VariableDeclaration variable, DeclarationBuilder builder);
+}
+
+/// The interface for [Macro]s that can be applied to any top level variable
+/// or instance field, and wants to augment the variable definition.
+abstract class VariableDefinitionMacro implements Macro {
+  FutureOr<void> buildDefinitionForFunction(
+      VariableDeclaration variable, VariableDefinitionBuilder builder);
+}
+
+/// The interface for [Macro]s that can be applied to any class, and wants to
+/// contribute new type declarations to the program.
+abstract class ClassTypesMacro implements Macro {
+  FutureOr<void> buildTypesForClass(
+      ClassDeclaration clazz, TypeBuilder builder);
+}
+
+/// The interface for [Macro]s that can be applied to any class, and wants to
+/// contribute new non-type declarations to the program.
+abstract class ClassDeclarationsMacro implements Macro {
+  FutureOr<void> buildDeclarationsForClass(
+      ClassDeclaration clazz, ClassDeclarationBuilder builder);
+}
+
+/// The interface for [Macro]s that can be applied to any class, and wants to
+/// augment the definitions of members on the class.
+abstract class ClassDefinitionMacro implements Macro {
+  FutureOr<void> buildDefinitionForClass(
+      ClassDeclaration clazz, ClassDefinitionBuilder builder);
+}
+
+/// The interface for [Macro]s that can be applied to any field, and wants to
+/// contribute new type declarations to the program.
+abstract class FieldTypesMacro implements Macro {
+  FutureOr<void> buildTypesForField(
+      FieldDeclaration field, TypeBuilder builder);
+}
+
+/// The interface for [Macro]s that can be applied to any field, and wants to
+/// contribute new type declarations to the program.
+abstract class FieldDeclarationsMacro implements Macro {
+  FutureOr<void> buildTypesForField(
+      FieldDeclaration field, ClassMemberDeclarationBuilder builder);
+}
+
+/// The interface for [Macro]s that can be applied to any field, and wants to
+/// augement the field definition.
+abstract class FieldDefinitionsMacro implements Macro {
+  FutureOr<void> buildDefinitionForField(
+      FieldDeclaration field, VariableDefinitionBuilder builder);
+}
+
+/// The interface for [Macro]s that can be applied to any method, and wants to
+/// contribute new type declarations to the program.
+abstract class MethodTypesMacro implements Macro {
+  FutureOr<void> buildTypesForMethod(
+      MethodDeclaration method, TypeBuilder builder);
+}
+
+/// The interface for [Macro]s that can be applied to any method, and wants to
+/// contribute new non-type declarations to the program.
+abstract class MethodDeclarationDeclarationsMacro implements Macro {
+  FutureOr<void> buildDeclarationsForMethod(
+      MethodDeclaration method, ClassMemberDeclarationBuilder builder);
+}
+
+/// The interface for [Macro]s that can be applied to any method, and wants to
+/// augment the function definition.
+abstract class MethodDefinitionMacro implements Macro {
+  FutureOr<void> buildDefinitionForMethod(
+      MethodDeclaration method, FunctionDefinitionBuilder builder);
+}
+
+/// The interface for [Macro]s that can be applied to any constructor, and wants
+/// to contribute new type declarations to the program.
+abstract class ConstructorTypesMacro implements Macro {
+  FutureOr<void> buildTypesForConstructor(
+      ConstructorDeclaration method, TypeBuilder builder);
+}
+
+/// The interface for [Macro]s that can be applied to any constructors, and
+/// wants to contribute new non-type declarations to the program.
+abstract class ConstructorDeclarationDeclarationsMacro implements Macro {
+  FutureOr<void> buildDeclarationsForConstructor(
+      ConstructorDeclaration method, ClassMemberDeclarationBuilder builder);
+}
+
+/// The interface for [Macro]s that can be applied to any constructor, and wants
+/// to augment the function definition.
+abstract class ConstructorDefinitionMacro implements Macro {
+  FutureOr<void> buildDefinitionForConstructor(
+      ConstructorDeclaration method, ConstructorDefinitionBuilder builder);
 }
--- working/macros/example/data_class.dart
+++ working/macros/example/data_class.dart
@@ -5,226 +5,232 @@ import '../api/code.dart';
 
 const dataClass = _DataClass();
 
-class _DataClass implements ClassMacro {
+class _DataClass implements ClassDeclarationsMacro, ClassDefinitionMacro {
   const _DataClass();
 
   @override
-  void visitClass(ClassDeclaration clazz, ClassContext context) {
-    autoConstructor.visitClass(clazz, context);
-    copyWith.visitClass(clazz, context);
-    hashCode.visitClass(clazz, context);
-    equality.visitClass(clazz, context);
-    toString.visitClass(clazz, context);
+  Future<void> buildDeclarationsForClass(
+      ClassDeclaration clazz, ClassDeclarationBuilder context) async {
+    await autoConstructor.buildDeclarationsForClass(clazz, context);
+    await copyWith.buildDeclarationsForClass(clazz, context);
+    hashCode.buildDeclarationsForClass(clazz, context);
+    equality.buildDeclarationsForClass(clazz, context);
+    toString.buildDeclarationsForClass(clazz, context);
+  }
+
+  @override
+  Future<void> buildDefinitionForClass(
+      ClassDeclaration clazz, ClassDefinitionBuilder builder) async {
+    await hashCode.buildDefinitionForClass(clazz, builder);
+    await equality.buildDefinitionForClass(clazz, builder);
+    await toString.buildDefinitionForClass(clazz, builder);
   }
 }
 
 const autoConstructor = _AutoConstructor();
 
-class _AutoConstructor implements ClassMacro {
+class _AutoConstructor implements ClassDeclarationsMacro {
   const _AutoConstructor();
 
   @override
-  void visitClass(ClassDeclaration clazz, ClassContext context) {
-    context.buildDeclarations((builder) async {
-      var constructors = await builder.constructorsOf(clazz);
-      if (constructors.any((c) => c.name == '')) {
+  Future<void> buildDeclarationsForClass(
+      ClassDeclaration clazz, ClassDeclarationBuilder builder) async {
+    var constructors = await builder.constructorsOf(clazz);
+    if (constructors.any((c) => c.name == '')) {
+      throw ArgumentError(
+          'Cannot generate an unnamed constructor because one already exists');
+    }
+
+    var parts = [Code.fromString('${clazz.name}({')];
+    // Add all the fields of `declaration` as named parameters.
+    var fields = await builder.fieldsOf(clazz);
+    for (var field in fields) {
+      var requiredKeyword = field.type.isNullable ? '' : 'required ';
+      parts.add(Code.fromString('\n${requiredKeyword}this.${field.name},'));
+    }
+
+    // Add all super constructor parameters as named parameters.
+    var superclass = await builder.superclassOf(clazz);
+    MethodDeclaration? superconstructor;
+    if (superclass != null) {
+      var superconstructor = (await builder.constructorsOf(superclass))
+          .firstWhereOrNull((c) => c.name == '');
+      if (superconstructor == null) {
         throw ArgumentError(
-            'Cannot generate an unnamed constructor because one already exists');
+            'Super class $superclass of $clazz does not have an unnamed '
+            'constructor');
       }
-
-      var parts = [Code.fromString('${clazz.name}({')];
-      // Add all the fields of `declaration` as named parameters.
-      var fields = await builder.fieldsOf(clazz);
-      for (var field in fields) {
-        var requiredKeyword = field.type.isNullable ? '' : 'required ';
-        parts.add(Code.fromString('\n${requiredKeyword}this.${field.name},'));
+      // We convert positional parameters in the super constructor to named
+      // parameters in this constructor.
+      for (var param in superconstructor.positionalParameters) {
+        var requiredKeyword = param.isRequired ? 'required' : '';
+        var defaultValue = param.defaultValue == null
+            ? ''
+            : Code.fromParts([' = ', param.defaultValue!]);
+        parts.add(Code.fromParts([
+          '\n$requiredKeyword${param.type.toCode()} ${param.name}',
+          defaultValue,
+          ',',
+        ]));
       }
-
-      // Add all super constructor parameters as named parameters.
-      var superclass = await builder.superclassOf(clazz);
-      MethodDeclaration? superconstructor;
-      if (superclass != null) {
-        var superconstructor = (await builder.constructorsOf(superclass))
-            .firstWhereOrNull((c) => c.name == '');
-        if (superconstructor == null) {
-          throw ArgumentError(
-              'Super class $superclass of $clazz does not have an unnamed '
-              'constructor');
-        }
-        // We convert positional parameters in the super constructor to named
-        // parameters in this constructor.
-        for (var param in superconstructor.positionalParameters) {
-          var requiredKeyword = param.isRequired ? 'required' : '';
-          var defaultValue = param.defaultValue == null
-              ? ''
-              : Code.fromParts([' = ', param.defaultValue!]);
-          parts.add(Code.fromParts([
-            '\n$requiredKeyword${param.type.toCode()} ${param.name}',
-            defaultValue,
-            ',',
-          ]));
-        }
+      for (var param in superconstructor.namedParameters) {
+        var requiredKeyword = param.isRequired ? '' : 'required ';
+        var defaultValue = param.defaultValue == null
+            ? ''
+            : Code.fromParts([' = ', param.defaultValue!]);
+        parts.add(Code.fromParts([
+          '\n$requiredKeyword${param.type.toCode()} ${param.name}',
+          defaultValue,
+          ',',
+        ]));
+      }
+    }
+    parts.add(Code.fromString('\n})'));
+    if (superconstructor != null) {
+      parts.add(Code.fromString(' : super('));
+      for (var param in superconstructor.positionalParameters) {
+        parts.add(Code.fromString('\n${param.name},'));
+      }
+      if (superconstructor.namedParameters.isNotEmpty) {
+        parts.add(Code.fromString('{'));
         for (var param in superconstructor.namedParameters) {
-          var requiredKeyword = param.isRequired ? '' : 'required ';
-          var defaultValue = param.defaultValue == null
-              ? ''
-              : Code.fromParts([' = ', param.defaultValue!]);
-          parts.add(Code.fromParts([
-            '\n$requiredKeyword${param.type.toCode()} ${param.name}',
-            defaultValue,
-            ',',
-          ]));
+          parts.add(Code.fromString('\n${param.name}: ${param.name},'));
         }
+        parts.add(Code.fromString('\n}'));
       }
-      parts.add(Code.fromString('\n})'));
-      if (superconstructor != null) {
-        parts.add(Code.fromString(' : super('));
-        for (var param in superconstructor.positionalParameters) {
-          parts.add(Code.fromString('\n${param.name},'));
-        }
-        if (superconstructor.namedParameters.isNotEmpty) {
-          parts.add(Code.fromString('{'));
-          for (var param in superconstructor.namedParameters) {
-            parts.add(Code.fromString('\n${param.name}: ${param.name},'));
-          }
-          parts.add(Code.fromString('\n}'));
-        }
-        parts.add(Code.fromString(')'));
-      }
-      parts.add(Code.fromString(';'));
-      builder.declareInClass(DeclarationCode.fromParts(parts));
-    });
+      parts.add(Code.fromString(')'));
+    }
+    parts.add(Code.fromString(';'));
+    builder.declareInClass(DeclarationCode.fromParts(parts));
   }
 }
 
 const copyWith = _CopyWith();
 
 // TODO: How to deal with overriding nullable fields to `null`?
-class _CopyWith implements ClassMacro {
+class _CopyWith implements ClassDeclarationsMacro {
   const _CopyWith();
 
   @override
-  void visitClass(ClassDeclaration clazz, ClassContext context) {
-    context.buildDeclarations((builder) async {
-      var methods = await builder.methodsOf(clazz);
-      if (methods.any((c) => c.name == 'copyWith')) {
-        throw ArgumentError(
-            'Cannot generate a copyWith method because one already exists');
-      }
-      var allFields = await clazz.allFields(builder).toList();
-      var namedParams = [
-        for (var field in allFields)
-          ParameterCode.fromString(
-              '${field.type.toCode()}${field.type.isNullable ? '' : '?'} '
-              '${field.name}'),
-      ];
-      var args = [
-        for (var field in allFields)
-          NamedArgumentCode.fromString(
-              '${field.name}: ${field.name} ?? this.${field.name}'),
-      ];
-      builder.declareInClass(DeclarationCode.fromParts([
-        clazz.instantiate().toCode(),
-        ' copyWith({',
-        ...namedParams.joinAsCode(', '),
-        ',})',
-        // TODO: We assume this constructor exists, but should check
-        '=> ', clazz.instantiate().toCode(), '(',
-        ...args.joinAsCode(', '),
-        ', );',
-      ]));
-    });
+  Future<void> buildDeclarationsForClass(
+      ClassDeclaration clazz, ClassDeclarationBuilder builder) async {
+    var methods = await builder.methodsOf(clazz);
+    if (methods.any((c) => c.name == 'copyWith')) {
+      throw ArgumentError(
+          'Cannot generate a copyWith method because one already exists');
+    }
+    var allFields = await clazz.allFields(builder).toList();
+    var namedParams = [
+      for (var field in allFields)
+        ParameterCode.fromString(
+            '${field.type.toCode()}${field.type.isNullable ? '' : '?'} '
+            '${field.name}'),
+    ];
+    var args = [
+      for (var field in allFields)
+        NamedArgumentCode.fromString(
+            '${field.name}: ${field.name} ?? this.${field.name}'),
+    ];
+    builder.declareInClass(DeclarationCode.fromParts([
+      clazz.instantiate().toCode(),
+      ' copyWith({',
+      ...namedParams.joinAsCode(', '),
+      ',})',
+      // TODO: We assume this constructor exists, but should check
+      '=> ', clazz.instantiate().toCode(), '(',
+      ...args.joinAsCode(', '),
+      ', );',
+    ]));
   }
 }
 
 const hashCode = _HashCode();
 
-class _HashCode implements ClassMacro {
+class _HashCode implements ClassDeclarationsMacro, ClassDefinitionMacro {
   const _HashCode();
 
   @override
-  void visitClass(ClassDeclaration clazz, ClassContext context) {
-    context.buildDeclarations((builder) {
-      builder.declareInClass(DeclarationCode.fromString('''
+  void buildDeclarationsForClass(
+      ClassDeclaration clazz, ClassDeclarationBuilder builder) {
+    builder.declareInClass(DeclarationCode.fromString('''
 @override
 external int get hashCode;'''));
-    });
+  }
 
-    context.buildDefinitions((builder) async {
-      await builder.buildMethod('hashCode', (builder) async {
-        var hashCodeExprs = [
-          await for (var field in clazz.allFields(builder))
-            ExpressionCode.fromString('${field.name}.hashCode')
-        ].joinAsCode(' ^ ');
-        builder.augment(FunctionBodyCode.fromParts([
-          ' => ',
-          ...hashCodeExprs,
-          ';',
-        ]));
-      });
-    });
+  @override
+  Future<void> buildDefinitionForClass(
+      ClassDeclaration clazz, ClassDefinitionBuilder builder) async {
+    var hashCodeBuilder = builder.buildMethod('hashCode');
+    var hashCodeExprs = [
+      await for (var field in clazz.allFields(builder))
+        ExpressionCode.fromString('${field.name}.hashCode')
+    ].joinAsCode(' ^ ');
+    hashCodeBuilder.augment(FunctionBodyCode.fromParts([
+      ' => ',
+      ...hashCodeExprs,
+      ';',
+    ]));
   }
 }
 
 const equality = _Equality();
 
-class _Equality implements ClassMacro {
+class _Equality implements ClassDeclarationsMacro, ClassDefinitionMacro {
   const _Equality();
 
   @override
-  void visitClass(ClassDeclaration clazz, ClassContext context) {
-    context.buildDeclarations((builder) async {
-      builder.declareInClass(DeclarationCode.fromString('''
+  void buildDeclarationsForClass(
+      ClassDeclaration clazz, ClassDeclarationBuilder builder) {
+    builder.declareInClass(DeclarationCode.fromString('''
 @override
 external bool operator==(Object other);'''));
-    });
+  }
 
-    context.buildDefinitions((builder) async {
-      await builder.buildMethod('==', (builder) async {
-        var equalityExprs = [
-          await for (var field in clazz.allFields(builder))
-            ExpressionCode.fromString(
-                'this.${field.name} == other.${field.name}'),
-        ].joinAsCode(' && ');
-        DeclarationCode.fromParts([
-          ' => other is ${clazz.instantiate().toCode()} && ',
-          ...equalityExprs,
-          ';',
-        ]);
-      });
-    });
+  @override
+  Future<void> buildDefinitionForClass(
+      ClassDeclaration clazz, ClassDefinitionBuilder builder) async {
+    var equalsBuilder = builder.buildMethod('==');
+    var equalityExprs = [
+      await for (var field in clazz.allFields(builder))
+        ExpressionCode.fromString('this.${field.name} == other.${field.name}'),
+    ].joinAsCode(' && ');
+    equalsBuilder.augment(FunctionBodyCode.fromParts([
+      ' => other is ${clazz.instantiate().toCode()} && ',
+      ...equalityExprs,
+      ';',
+    ]));
   }
 }
 
 const toString = _ToString();
 
-class _ToString implements ClassMacro {
+class _ToString implements ClassDeclarationsMacro, ClassDefinitionMacro {
   const _ToString();
 
   @override
-  void visitClass(ClassDeclaration clazz, ClassContext context) {
-    context.buildDeclarations((builder) async {
-      builder.declareInClass(DeclarationCode.fromString(
-        '''
+  void buildDeclarationsForClass(
+      ClassDeclaration clazz, ClassDeclarationBuilder builder) {
+    builder.declareInClass(DeclarationCode.fromString(
+      '''
 @override
 external String toString();''',
-      ));
-    });
+    ));
+  }
 
-    context.buildDefinitions((builder) async {
-      await builder.buildMethod('toString', (builder) async {
-        var fieldExprs = [
-          await for (var field in clazz.allFields(builder))
-            Code.fromString('  ${field.name}: \${${field.name}}'),
-        ].joinAsCode('\n');
+  @override
+  Future<void> buildDefinitionForClass(
+      ClassDeclaration clazz, ClassDefinitionBuilder builder) async {
+    var toStringBuilder = builder.buildMethod('toString');
+    var fieldExprs = [
+      await for (var field in clazz.allFields(builder))
+        Code.fromString('  ${field.name}: \${${field.name}}'),
+    ].joinAsCode('\n');
 
-        builder.augment(FunctionBodyCode.fromParts([
-          ' => """\${${clazz.name}} { ',
-          ...fieldExprs,
-          '}""";',
-        ]));
-      });
-    });
+    toStringBuilder.augment(FunctionBodyCode.fromParts([
+      ' => """\${${clazz.name}} { ',
+      ...fieldExprs,
+      '}""";',
+    ]));
   }
 }
 
