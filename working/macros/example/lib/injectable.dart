ca102107bb7bb18774272354f5c553b97f850049
Jake Macdonald <jakemac@google.com>
2023-04-22 02:53:47 +0000

    move comment back

--- working/macros/example/lib/injectable.dart
+++ working/macros/example/lib/injectable.dart
@@ -2,9 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// There is no public API exposed yet, the in progress api lives here.
 import 'dart:async';
 
+// There is no public API exposed yet, the in progress api lives here.
 import 'package:_fe_analyzer_shared/src/macros/api.dart';
 
 import 'util.dart';
