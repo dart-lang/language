f387ccf05b970cb2a6e351e2579ec8fa944574e1
Jake Macdonald <jakemac@google.com>
2024-01-23 17:20:35 +0000

    add copyrights

--- working/macros/example/bin/auto_dispose_main.dart
+++ working/macros/example/bin/auto_dispose_main.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2024, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:macro_proposal/auto_dispose.dart';
 
 void main() {
