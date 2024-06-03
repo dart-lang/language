dd089ef870a9c95729ef7766cd81758f3bb1fe53
David Morgan <davidmorgan@google.com>
2024-06-03 11:07:21 +0200

    [macros] Add ClassGeneratorMacro.

--- working/macros/dart_model/testing/test_macros/lib/equals_macro.dart
+++ working/macros/dart_model/testing/test_macros/lib/equals_macro.dart
@@ -2,52 +2,15 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
-
 import 'package:dart_model/model.dart';
-import 'package:dart_model/query.dart';
-import 'package:macro_client/macro.dart';
-import 'package:macro_protocol/host.dart';
-import 'package:macro_protocol/message.dart';
+import 'package:macro_client/class_generator_macro.dart';
 
-class EqualsMacro implements Macro {
-  final Model model = Model();
-
+class EqualsMacro extends ClassGeneratorMacro {
   @override
   QualifiedName get name => QualifiedName(
-      uri: 'package:test_macros/equals_macro.dart', name: 'EqualsMacro');
+      uri: 'package:test_macros_annotations/annotations.dart', name: 'Equals');
 
   @override
-  Future<void> start(Host host) async {
-    final stream = await host.watch(Query.annotation(QualifiedName(
-      uri: 'package:test_macro_annotations/annotations.dart',
-      name: 'Equals',
-    )));
-    stream.listen((round) => generate(host, round));
-  }
-
-  void generate(Host host, Round round) async {
-    final delta = round.delta;
-    delta.update(model);
-
-    final augmentationsByUri = <String, String>{};
-    for (final uri in delta.uris) {
-      final library = model.library(uri)!;
-      final result = StringBuffer();
-      for (final scope in library.scopes) {
-        final clazz = scope.asInterface!;
-        result.write(generateFor(clazz));
-      }
-
-      augmentationsByUri[uri] = result.toString();
-    }
-
-    unawaited(host.augment(
-        macro: name,
-        round: round.round,
-        augmentationsByUri: augmentationsByUri));
-  }
-
   String generateFor(Interface clazz) {
     final result = StringBuffer();
 
