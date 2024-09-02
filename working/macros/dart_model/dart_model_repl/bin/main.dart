0af0a920b26cab887e8e73ecc4a5412a3f4f5626
David Morgan <davidmorgan@google.com>
2024-09-02 10:40:54 +0200

    Remove dart_model experimental code from language repo. See macros repo.

--- working/macros/dart_model/dart_model_repl/bin/main.dart
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2024, the Dart project authors. Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
-import 'package:dart_model_repl/dart_model_repl.dart';
-
-Future<void> main() async {
-  await DartModelRepl().run();
-}
